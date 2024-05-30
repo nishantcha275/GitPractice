b87e75ab0c90b70c372e184aa7c02d37bf652dd7
Nishant <Nishant@DESKTOP-D0HDFK6>
2024-05-30 14:36:04 +0530

    latest

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,string> dic = new Dictionary<string,string>();
+            dic.Add("A", "Apple");
+            dic.Add("B", "Boy");
+            dic.Add("C", "Cat");
+            dic.Add("D", "Dog");
+            dic.Add("E", "Elephant");
+            foreach(KeyValuePair<string,string> item in dic) 
+            {
+                Console.WriteLine("key is: " + item.Key+ "\t" + "value is: "+ item.Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
