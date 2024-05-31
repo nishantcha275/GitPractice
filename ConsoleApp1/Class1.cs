7d474f82109d810037dccd3dd94121072e33c0d3
Nishant <Nishant@DESKTOP-D0HDFK6>
2024-05-31 11:51:42 +0530

    add class 1

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        public void add(int a, int b)
+        {
+            Console.WriteLine(a+b);
+        }
+        public static void main(string[] args)
+        {
+           Class1 class1 = new Class1();
+           class1.add(1,2);
+        }
+    }
+}
